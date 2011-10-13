a904f557d323a1bfdcfa86e99e9d38bc9a3516ba
rgan <rajaram.ganeshan@gmail.com>
2011-10-12 22:15:19 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dothespread::Application.config.secret_token = '86c7b61bbf6c9748e9daa498e0040a756dc5486aa51b41a777a048ac97fc99f43d1035907815f40307f5cbcb1f896baf69b2355948642623cd334fce65f42b21'
---
+++ public/js/registrations.js
@@ -0,0 +1,8 @@
+var registrations = [
+    new google.maps.LatLng(40.7275770, -74.002720),
+    new google.maps.LatLng(40.7176771, -74.0002719),
+    new google.maps.LatLng(40.7275770, -74.0002717),
+    new google.maps.LatLng(40.7375770, -74.0102729),
+    new google.maps.LatLng(40.7675770, -74.1102729),
+    new google.maps.LatLng(40.7775770, -74.2102729)
+];

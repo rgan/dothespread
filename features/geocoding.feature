d5461de099a4db8372d5f0199063f225566eb3fa
rgan <rajaram.ganeshan@gmail.com>
2011-10-17 10:33:57 -0400

    Removing registration related code

--- features/geocoding.feature
+++
@@ -1,4 +0,0 @@
-Feature: Geocode
-  Scenario: Should geocode address
-    When I geocode 1600 Amphitheatre Parkway, Mountain View, CA
-    Then I should get latitude 37 and longitude -122
--- features/step_definitions/geocoding_steps.rb
+++
@@ -1,8 +0,0 @@
-When /^I geocode ([^,]*), ([^,]*), ([^,]*)$/ do |streetAddress, city, country|
-  @location = Geocoder.geocode(streetAddress, city, country)
-end
-
-Then /^I should get latitude ([0-9.-]+) and longitude ([0-9.-]+)$/ do |lat, lng|
-  @location.latitude.to_i.should == lat.to_i
-  @location.longitude.to_i.should == lng.to_i
-end

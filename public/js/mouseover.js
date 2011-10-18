ea7721b52195fb783b347a2f06815722c078897f
Shaun McGee <smcgee@thoughtworks.com>
2011-10-18 18:28:09 -0400

    testing social icons

---
+++ public/js/mouseover.js
@@ -0,0 +1,14 @@
+function mouseOver()
+{
+	document.getElementById("em1").src ="images/email-red-mo.png"
+	document.getElementById("fb1").src ="images/facebook-red-mo.png"
+	document.getElementById("tw1").src ="images/twitter-red-mo.png"
+	document.getElementById("gp1").src ="images/google-red-mo.png"
+}
+function mouseOut()
+{
+	document.getElementById("em1").src ="images/email-red.png"
+	document.getElementById("fb1").src ="images/facebook-red.png"
+	document.getElementById("tw1").src ="images/twitter-red.png"
+	document.getElementById("gp1").src ="images/google-red.png"
+}

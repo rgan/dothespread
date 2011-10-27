f8ecae680e5a2dae554dce36a47502581c377429
rgan <rajaram.ganeshan@gmail.com>
2011-10-27 19:41:00 -0400

    Removed unneeded format handling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,6 @@ class HomeController < ApplicationController
 
   def index
     response.headers['Cache-Control'] = CACHE_CONTROL_VALUE
-    respond_to do |format|
-      format.html # index.html.erb
-      format.mobile # index.mobile.erb
-    end
   end
 
   def endorsements

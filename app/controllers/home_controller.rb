0807a71402a99220874512c358727fdd002a2fe2
rgan <rajaram.ganeshan@gmail.com>
2011-10-30 09:45:45 -0400

    Disable cache-control header

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
 
   def index
-    response.headers['Cache-Control'] = CACHE_CONTROL_VALUE
+    #response.headers['Cache-Control'] = CACHE_CONTROL_VALUE
   end
 
   def endorsements
-    response.headers['Cache-Control'] = CACHE_CONTROL_VALUE
+    #response.headers['Cache-Control'] = CACHE_CONTROL_VALUE
   end
 end

b5de5ade81d2938749a000c6cfb0a94cd722fceb
rgan <rajaram.ganeshan@gmail.com>
2011-11-04 11:41:28 -0400

    Make facebook app id configurable via env variable

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+FACEBOOK_APP_ID = ENV["FACEBOOK_APP_ID"]
+
 # Initialize the rails application
 Dothespread::Application.initialize!

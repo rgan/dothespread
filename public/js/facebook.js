197613112b309578e4591759849c10c59d4b7570
rgan <rajaram.ganeshan@gmail.com>
2011-11-04 12:10:40 -0400

    Added event permission; commented out rsvp for testing

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -1,21 +1,16 @@
 function rsvp() {
-    FB.getLoginStatus(function(response) {
-      if (response.authResponse) {
-        doRsvp();
-      } else {
-        FB.login(function(response) {
-            if (response.authResponse) {
-                doRsvp();
-            }
-        });
-      }
-    });
+    FB.login(function(response) {
+        if (response.authResponse) {
+            doRsvp();
+        }
+    }, { scope: 'rsvp_event' });
 }
 
 function doRsvp() {
-    FB.api('/265793463456799/attending', 'post', { message: ''}, function(response) {
+ /*   FB.api('/265793463456799/attending', 'post', { message: ''}, function(response) {
         if (!response || response.error) {
             alert('Error RSVPing for event');
         }
     });
+    */
 }

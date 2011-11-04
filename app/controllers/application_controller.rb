9bba013408587dc53d2bf2ee4649a32bd8e0317c
rgan <rajaram.ganeshan@gmail.com>
2011-11-04 12:19:02 -0400

    Uncommented facebook rsvp; increased cache expiry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  CACHE_CONTROL_VALUE='public, max-age=300'
+  CACHE_CONTROL_VALUE='public, max-age=3600'
   before_filter :set_mobile_format
 
   def set_mobile_format
--- public/js/facebook.js
+++ public/js/facebook.js
@@ -7,10 +7,10 @@ function rsvp() {
 }
 
 function doRsvp() {
- /*   FB.api('/265793463456799/attending', 'post', { message: ''}, function(response) {
+    FB.api('/265793463456799/attending', 'post', { message: ''}, function(response) {
         if (!response || response.error) {
             alert('Error RSVPing for event');
         }
     });
-    */
+
 }

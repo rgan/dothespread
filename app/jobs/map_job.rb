f3c11e015352ae85bcbb3f798fc25e25de3f2baf
Shaun McGee <smcgee@thoughtworks.com>
2011-11-07 11:30:25 -0500

    updated map style and FB/Twitter links

--- app/jobs/map_job.rb
+++ app/jobs/map_job.rb
@@ -8,9 +8,9 @@ class MapJob
   end
 
   def getMap(locations)
-    map_url = "http://maps.googleapis.com/maps/api/staticmap?zoom=1&size=575x286&maptype=roadmap&markers=color:blue&sensor=false"
+    map_url = "http://maps.googleapis.com/maps/api/staticmap?center=45,5&zoom=1&format=png&sensor=false&size=500x350&maptype=roadmap&style=feature:water|visibility:on|hue:0xff1100|lightness:100&style=feature:administrative|hue:0x0000ff|visibility:off&style=feature:water|element:labels|visibility:off&style=feature:poi|visibility:off&style=feature:landscape|element:geometry|visibility:on|hue:0x00ff19|lightness:-55"
     markers = locations.collect { |location| location.latitude + "," + location.longitude}.join("|")
-    map_url = map_url + "&markers=" + URI.escape(markers)
+    map_url = map_url + "&markers=size:tiny%7Ccolor:red%7C" + URI.escape(markers)
     uri = URI.parse(map_url)
     http = Net::HTTP.new(uri.host, uri.port)
     begin

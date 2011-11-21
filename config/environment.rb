cf84a5706ecc53dab70addd943b1106e8cd18a54
rgan <rajaram.ganeshan@gmail.com>
2011-11-20 23:07:47 -0500

    Get bucket name from env

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 FACEBOOK_APP_ID = ENV["FACEBOOK_APP_ID"]
+S3_BUCKET=ENV['AWS_BUCKET']
 
 # Initialize the rails application
 Dothespread::Application.initialize!
